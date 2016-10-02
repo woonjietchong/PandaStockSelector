ec96494b2047e3a7e3d0500a174b8ca3191ebbdc
woonjietchong <vertwj@gmail.com>
2016-10-02 11:56:25 +0800

    3rd_capable_to_read_all_stocks_and_analyze

--- yahoo_finance_data_extract/financial_example.py
+++ yahoo_finance_data_extract/financial_example.py
@@ -20,32 +20,40 @@ if __name__ == '__main__':
         
         if stock_designated == "searchdb":
             
+            #retrive database from text file
             klse_df = pandas.read_csv('klse_stocks_25_09_2016.csv')     
             
             print klse_df
             
-            #retrive database from text file
+            
         
-            number_of_stocks = raw_input("Enter number of stock you want to analyse: ")
+            number_of_stocks = klse_df['stock_code']
+            
+            print number_of_stocks
+            print number_of_stocks.size
 
             stocks_array = []
-            for i in range( int(number_of_stocks) ):
-                entered_stock_code = raw_input("Enter stock code ["+str(i)+"] :" )
-                stocks_array.append(entered_stock_code)
+            for i in range( number_of_stocks.size ):
+                print klse_df['stock_code'][i]
+                stocks_array.append( str( klse_df['stock_code'][i] ) )
                 
             print stocks_array
             
             
             
-            for i in range( int(number_of_stocks) ):
-                
+            for i in range( number_of_stocks.size ):                
                 print "----------------" + stocks_array[i] + "----------------"
                 data_ext = YFHistDataExtr()
                 data_ext.set_interval_to_retrieve(200)#in days
                 data_ext.set_multiple_stock_list([stocks_array[i]])
                 data_ext.get_hist_data_of_all_target_stocks()
                 # convert the date column to date object
-                data_ext.all_stock_df['Date'] =  pandas.to_datetime( data_ext.all_stock_df['Date'])
+                try:
+                    data_ext.all_stock_df['Date'] =  pandas.to_datetime( data_ext.all_stock_df['Date'])
+                except TypeError:
+                    print "TypeError, skip this stock" + str( klse_df['stock_code'][i] )
+                    continue
+                #data_ext.all_stock_df['Date'] =  pandas.to_datetime( data_ext.all_stock_df['Date'])
                 temp_data_set = data_ext.all_stock_df.sort('Date',ascending = True ) #sort to calculate the rolling mean
                 
                 temp_data_set['20d_ma'] = pandas.rolling_mean(temp_data_set['Adj Close'], window=20)
@@ -60,21 +68,23 @@ if __name__ == '__main__':
                 data_ext.all_stock_df = temp_data_set.sort('Date',ascending = False ) #revese back to original
                  
                 #data_ext.all_stock_df.plot(x='Date', y=['Adj Close','20d_ma','50d_ma','Bol_upper','Bol_lower' ])
-                data_ext.all_stock_df.plot(x='Date', y=['Adj Close','20d_ma','Bol_upper','Bol_lower' ])
+                #data_ext.all_stock_df.plot(x='Date', y=['Adj Close','20d_ma','Bol_upper','Bol_lower' ])
                 #data_ext.all_stock_df.plot(x='Date', y=['Bol_BW','Bol_BW_200MA' ])
                 
-                print temp_data_set['Adj Close']
-                print temp_data_set['Bol_upper']
+
                 
-                print "type of temp_data_set['Adj Close'] is " + str( type(temp_data_set['Adj Close']) )
-                print "type of temp_data_set['Bol_upper'] is " + str( type(temp_data_set['Bol_upper']) )
+#                print "type of temp_data_set['Adj Close'] is " + str( type(temp_data_set['Adj Close']) )
+#                print "type of temp_data_set['Bol_upper'] is " + str( type(temp_data_set['Bol_upper']) )
+#                
+#                print  "temp_data_set['Adj Close'][0] is " + str( temp_data_set['Adj Close'][0] )
+#                print  "temp_data_set['Bol_upper'][0] is " + str( temp_data_set['Bol_upper'][0] )
                 
-                print  "temp_data_set['Adj Close'][0] is " + str( temp_data_set['Adj Close'][0] )
-                print  "temp_data_set['Bol_upper'][0] is " + str( temp_data_set['Bol_upper'][0] )             
-                
-                plt.show()
-                
-                print "--------------------------------------------------------"
+                if temp_data_set['Adj Close'][0] > temp_data_set['Bol_upper'][0] :
+                    print "------------------------------------------------------"
+                    print "---------found   " + klse_df['stock_name'][i] + " matched-------"
+                    data_ext.all_stock_df.plot(x='Date', y=['Adj Close','20d_ma','Bol_upper','Bol_lower' ])
+                    plt.show()
+                    print "--------------------------------------------------------"
             
         else:
             data_ext = YFHistDataExtr()
