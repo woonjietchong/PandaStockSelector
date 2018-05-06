ea0b6cc070d800d527a6fc639dc45c49bce1fbeb
woonjietchong <vertwj@gmail.com>
2018-05-06 17:43:10 +0800

    work on may 6 able to commit into database

--- PandaStockSelector/main/main_module.py
+++ PandaStockSelector/main/main_module.py
@@ -8,12 +8,6 @@ import Tkinter
 from tika import parser
 import re
 import sqlite3
-import csv
-
-#----- Globals variables ----------
-stock_symbols = []
-stock_symbol_reader = None
-conn_closed = False
 
 class simpleapp_tk(Tkinter.Tk):
     def __init__(self,parent):
@@ -22,6 +16,7 @@ class simpleapp_tk(Tkinter.Tk):
         self.initialize()
 
     def initialize(self):
+        self.conn_closed = False
         self.grid()
 
         self.entryVariable = Tkinter.StringVar()
@@ -61,118 +56,136 @@ class simpleapp_tk(Tkinter.Tk):
 
         regexMatches = re.findall(r"([0-9][0-9])/([0-9][0-9])/([0-9][0-9][0-9][0-9]) ([0-9][0-9][0-9][0-9][A-Z0-9]??[A-Z0-9]??) (.*) MYR (.*) (.*) (.*) (.*) (.*) (.*)", parsedContent)#, re.MULTILINE)
         #regexMatches = re.match(r"([0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]) ([0-9][0-9][0-9][0-9][A-Z0-9]??[A-Z0-9]??) (.*) MYR (.*) (.*) (.*) (.*) (.*) (.*)", parsedContent)
-        print("regexMatches.groups()")        
-        for groupsInRegex in regexMatches:            
-            print("----------")
-            print(groupsInRegex[0])
-            print(groupsInRegex[1])
-            print(groupsInRegex[2])
-            print(groupsInRegex[3])
-            print(groupsInRegex[4])
-            print(groupsInRegex[5])
-            print(groupsInRegex[6])
-            print(groupsInRegex[7])
-            print(groupsInRegex[8])
-            print(groupsInRegex[9])
-            print(groupsInRegex[10])
-            print("----------")
+
+        #for groupsInRegex in regexMatches:            
+            #print("----------")
+            #print(groupsInRegex[0]) #--> Date
+            #print(groupsInRegex[1]) #--> Month
+            #print(groupsInRegex[2]) #--> Year
+            #print(groupsInRegex[3]) #--> Stock Code
+            #print(groupsInRegex[4]) #--> Stock Name
+            #print(groupsInRegex[5]) #--> Opening Price
+            #print(groupsInRegex[6]) #--> High Price
+            #print(groupsInRegex[7]) #--> Low Price
+            #print(groupsInRegex[8]) #--> Closing Price
+            #print(groupsInRegex[9]) #--> Volume Traded
+            #print(groupsInRegex[10]) #--> Value Traded
+            #print("----------")
             
-#        print "Starting wj stock db input"
-#        conn = sqlite3.connect('klse_from_bursa.db')
-#        print "after conn connect"
-#        try:
-#            c = conn.cursor()
-#            print "after conn.cursor"
-#    
-#            # Create table
-#            # Database Schema
-#            #   symbol	            TEXT
-#            #   name                TEXT
-#            #   date-YYYY	        INTEGER
-#            #   date-MM             INTEGER
-#            #   date-DD             INTEGER
-#            #   open_price          NUMERIC -> decimal 3, 2
-#            #   high_price          NUMERIC -> decimal 3, 2
-#            #   low_price	        NUMERIC -> decimal 3, 2
-#            #   close_price	        NUMERIC -> decimal 3, 2
-#            #   volume	            INTEGER -> unsigned bigInt
-#            #   value_traded	      INTEGER -> unsigned bigInt
-#            
-#            #c.execute('''CREATE TABLE IF NOT EXISTS stocks (symbol text, date text, price real)''')
-#            c.execute('''CREATE TABLE IF NOT EXISTS stocks (symbol text, name text, date_YYYY integer, date_MM integer, date_DD integer, open_price numeric, high_price numeric, low_price numeric, close_price numeric, volume integer, value_traded integer)''')
-#            print "after CREATE TABLE"
-#            
-#            print "getting finance data from yahoo"
-#            for groupsInRegex in regexMatches:            
-#                print("----------")
-#                current_stock_symbol = iterated_symbol
-#                current_stock_name = current_stock.get_name()               #TEXT
-#                
-#                temp_trade_datetime = current_stock.get_trade_datetime()	#TEXT
-#                  # for future reference : example of get_trade_datetime() are 2017-06-23 20:17:00 UTC+0000
-#                current_stock_date_YYYY	= temp_trade_datetime[0:4]        #INTEGER
-#                current_stock_date_MM = temp_trade_datetime[5:7]            #INTEGER
-#                current_stock_date_DD = temp_trade_datetime[8:10]            #INTEGER
-#                
-#                current_stock_open_price = current_stock.get_open()         #NUMERIC -> decimal 3, 2
-#                print "after get_open"
-#                current_stock_high_price = current_stock.get_days_high()         #NUMERIC -> decimal 3, 2
-#                print "after get_days_high"
-#                current_stock_low_price	= current_stock.get_days_low()        #NUMERIC -> decimal 3, 2
-#                print "after get_days_low"
-#                current_stock_close_price = current_stock.get_price()	        #NUMERIC -> decimal 3, 2
-#                print "after get_price"
-#                current_stock_volume = current_stock.get_volume()	            #INTEGER -> unsigned bigInt
-#                print "after get_volume"
-#                current_stock_value_traded = current_stock.get_value_traded()	            #INTEGER -> unsigned bigInt
-#                print "after get_volume"
-#                
-#                #First
-#                db_query_string = "INSERT INTO stocks VALUES (" + "'" + current_stock_symbol + "'" 
-#                print "after first"
-#                db_query_string = db_query_string + ", " + "'" + current_stock_name + "'"
-#                db_query_string = db_query_string + ", " + current_stock_date_YYYY
-#                db_query_string = db_query_string + ", " + current_stock_date_MM
-#                db_query_string = db_query_string + ", " + current_stock_date_DD                   
-#                db_query_string = db_query_string + ", " + current_stock_open_price
-#                db_query_string = db_query_string + ", " + current_stock_high_price
-#                db_query_string = db_query_string + ", " + current_stock_low_price
-#                db_query_string = db_query_string + ", " + current_stock_close_price
-#                db_query_string = db_query_string + ", " + current_stock_volume
-#                db_query_string = db_query_string + ", " + current_stock_value_traded
-#
-#                #Last                
-#                db_query_string = db_query_string +")"
-#    
-#                print db_query_string
-#                
-#            try:
-#                # Insert a row of data
-#                c.execute(db_query_string)
-#                db_query_success = True
-#            except Exception, e:
-#                print "Query Execution failure"
-#                print e
-#                db_query_success = False
-#                
-#            if(db_query_success):
-#                # Save (commit) the changes
-#                print "Saving changes to database"
-#                conn.commit()
-#                            
-#            
-#        except Exception, e:
-#            #cleanup to prevent database not accessible later
-#            print "General database failure , now cleanup"
-#            print e
-#            conn.close()
-#            conn_closed = True
-#        
-#        # We can also close the connection if we are done with it.
-#        # Just be sure any changes have been committed or they will be lost.
-#        if(not conn_closed):
-#            print "Closing connection"
-#            conn.close()
+        print "Starting wj stock db input"
+        conn = sqlite3.connect('klse_from_bursa.db')
+        print "after conn connect"
+        try:
+            c = conn.cursor()
+            print "after conn.cursor"
+    
+            # Create table
+            # Database Schema
+            #   symbol	            TEXT
+            #   name                TEXT
+            #   date-YYYY	        INTEGER
+            #   date-MM             INTEGER
+            #   date-DD             INTEGER
+            #   open_price          NUMERIC -> decimal 3, 2
+            #   high_price          NUMERIC -> decimal 3, 2
+            #   low_price	        NUMERIC -> decimal 3, 2
+            #   close_price	        NUMERIC -> decimal 3, 2
+            #   volume	            INTEGER -> unsigned bigInt
+            #   value_traded	      INTEGER -> unsigned bigInt
+            
+            #c.execute('''CREATE TABLE IF NOT EXISTS stocks (symbol text, date text, price real)''')
+            c.execute('''CREATE TABLE IF NOT EXISTS stocks (symbol text, name text, date_YYYY integer, date_MM integer, date_DD integer, open_price numeric, high_price numeric, low_price numeric, close_price numeric, volume integer, value_traded integer)''')
+            print "after CREATE TABLE"
+            
+            for groupsInRegex in regexMatches:            
+                #print("----------")
+                current_stock_symbol = groupsInRegex[3]                  #TEXT
+                current_stock_name = groupsInRegex[4]                    #TEXT
+                current_stock_date_YYYY	= groupsInRegex[2]          #INTEGER
+                current_stock_date_MM = groupsInRegex[1]            #INTEGER
+                current_stock_date_DD = groupsInRegex[0]            #INTEGER
+                
+                try:
+                    current_stock_open_price = str(float(groupsInRegex[5]))         #NUMERIC -> decimal 3, 2
+                    #print "after get_open"
+                except Exception, e:
+                    print current_stock_name + " conversion failure"
+                    print e
+                    current_stock_open_price = "0.0"
+                
+                try:
+                    current_stock_high_price = str(float(groupsInRegex[6]))        #NUMERIC -> decimal 3, 2
+                    #print "after get_days_high"
+                except Exception, e:
+                    print current_stock_name + " conversion failure"
+                    print e
+                    current_stock_high_price = "0.0"
+                    
+                try:
+                    current_stock_low_price	= str(float(groupsInRegex[7]))        #NUMERIC -> decimal 3, 2
+                    #print "after get_days_low"
+                except Exception, e:
+                    print current_stock_name + " conversion failure"
+                    print e
+                    current_stock_low_price = "0.0"
+                    
+                try:
+                    current_stock_close_price = str(float(groupsInRegex[8]))	      #NUMERIC -> decimal 3, 2
+                    #print "after get_price"
+                except Exception, e:
+                    print current_stock_name + " conversion failure"
+                    print e
+                    current_stock_close_price = "0.0"
+
+                current_stock_volume = groupsInRegex[9]	           #INTEGER -> unsigned bigInt
+                #print "after get_volume"
+                current_stock_value_traded = groupsInRegex[10]	      #INTEGER -> unsigned bigInt
+                #print "after get_value_traded"
+                
+                #First
+                db_query_string = "INSERT INTO stocks VALUES (" + "'" + current_stock_symbol + "'" 
+                db_query_string = db_query_string + ", " + "'" + current_stock_name + "'"
+                db_query_string = db_query_string + ", " + current_stock_date_YYYY
+                db_query_string = db_query_string + ", " + current_stock_date_MM
+                db_query_string = db_query_string + ", " + current_stock_date_DD                   
+                db_query_string = db_query_string + ", " + current_stock_open_price
+                db_query_string = db_query_string + ", " + current_stock_high_price
+                db_query_string = db_query_string + ", " + current_stock_low_price
+                db_query_string = db_query_string + ", " + current_stock_close_price
+                db_query_string = db_query_string + ", " + current_stock_volume
+                db_query_string = db_query_string + ", " + current_stock_value_traded
+
+                #Last                
+                db_query_string = db_query_string +")"
+    
+                #print "printing db_query_string"
+                #print db_query_string
+                
+                try:
+                    # Insert a row of data
+                    c.execute(db_query_string)
+                except Exception, e:
+                    print "Query Execution failure"
+                    print(db_query_string)
+                    print e
+                    
+
+            # Save (commit) the changes
+            print "Saving changes to database"
+            conn.commit()
+            
+        except Exception, e:
+            #cleanup to prevent database not accessible later
+            print "General database failure , now cleanup"
+            print e
+            conn.close()
+            self.conn_closed = True
+        
+        # We can also close the connection if we are done with it.
+        # Just be sure any changes have been committed or they will be lost.
+        if(not self.conn_closed):
+            print "Closing connection"
+            conn.close()
 
         self.labelVariable.set( self.entryVariable.get()+" (You clicked the button)" )
         self.entry.focus_set()
