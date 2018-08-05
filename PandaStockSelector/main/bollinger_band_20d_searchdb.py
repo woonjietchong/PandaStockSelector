a6709033d2e09e96e63e6e7ef29e05868d7a8a2a
woonjietchong <vertwj@gmail.com>
2018-08-05 12:18:00 +0800

    working bollinger_band_20d_searchdb.py

--- PandaStockSelector/main/bollinger_band_20d_searchdb.py
+++ PandaStockSelector/main/bollinger_band_20d_searchdb.py
@@ -12,7 +12,65 @@ Created on Sun Jul 22 11:29:48 2018
 import pandas
 import sqlite3
 import matplotlib.pyplot as plt
- 
+
+#database variables
+db_connected = False
+conn = None
+query = None
+conn_target_path = 'C:\Users\Admin\Desktop\PandaStockSelectorWorkspace\PandaStockSelector\main\klse_from_bursa_dup.db'
+
+def connect_to_database():
+    global conn
+    if not db_connected:
+        conn = sqlite3.connect(conn_target_path)
+
+def get_single_stock_db(stock_designated):
+    #print "in get_single_stock_db"
+    global query
+    
+    #append ' ' to stock_designated to prevent database error
+    stock_designated = "'" + stock_designated + "'"
+    
+    if not db_connected:    
+        connect_to_database()
+    query = "SELECT symbol,name,date_YYYY,date_MM,date_DD,open_price,high_price,low_price,close_price,volume,value_traded FROM stocks WHERE symbol = " + stock_designated + ";"
+    target_df = pandas.read_sql_query(query,conn)
+    return target_df
+    
+def add_zero_padding_to_date_in_df(df):
+    #Init date column in dataframe
+    #print("--- add_zero_padding_to_date_in_df---------------- df contains")
+    #print df
+    #print("--- add_zero_padding_to_date_in_df---------------- df ends")
+            
+    for i in range( df['symbol'].size ):        
+        try:
+            #print "----------------" + df['symbol'][i] + "-- construct date ----------------"
+            #print(df['date_YYYY'][i])
+            #print(df['date_MM'][i])
+            #print(df['date_DD'][i])
+            #print(str(df['date_YYYY'][i])+"/"+str(df['date_MM'][i])+"/"+str(df['date_DD'][i]))
+            df.iat[i,11] = str(df['date_YYYY'][i]).zfill(4) +"/"+str(df['date_MM'][i]).zfill(2) +"/"+str(df['date_DD'][i]).zfill(2)
+        except Exception as e:
+            print "error in add_zero_padding_to_date_in_df for loop " + str(e)
+                    
+    #print("--- add_zero_padding_to_date_in_df---------------- df contains")
+    #print df
+    #print("--- add_zero_padding_to_date_in_df---------------- df ends")
+    return df
+    
+def init_additional_fields_in_df(df):
+    df = df.assign(Date="0")
+    df = df.assign(ma20d=df.close_price)
+    df = df.assign(ma50d=df.close_price)
+    df = df.assign(Bol_upper=df.close_price)
+    df = df.assign(Bol_lower=df.close_price)
+    df = df.assign(Bol_BW=0.0)
+    df = df.assign(Bol_BW_200MA=0.0)
+    df = df.assign(exma20d=df.close_price)
+    df = df.assign(exma50d=df.close_price)
+    return df
+
 if __name__ == '__main__':
     
         print "-----------Showing plot------------------------"
@@ -22,127 +80,56 @@ if __name__ == '__main__':
         if start_searching != None:
             start_searching = True
             
-            #retrive database from text file
+            #retrive stock symbol from database
             print("---------------- reading database")
-            conn = sqlite3.connect('C:\Users\Admin\Desktop\PandaStockSelectorWorkspace\PandaStockSelector\main\klse_from_bursa_dup.db')
-            all_db_query = "SELECT symbol,name,date_YYYY,date_MM,date_DD,open_price,high_price,low_price,close_price,volume,value_traded FROM stocks"
+            connect_to_database()
             stock_symbol_query =  "SELECT DISTINCT symbol FROM stocks"
- 
-            klse_all_db_df = pandas.read_sql_query(all_db_query,conn)
+            #stock_symbol_query =  "SELECT DISTINCT symbol FROM stocks WHERE symbol = 2089;"            
             klse_stock_symbol_df = pandas.read_sql_query(stock_symbol_query,conn)
             print("---------------- end of reading database")
-            
-            #Init date column in dataframe
-            klse_all_db_df = klse_all_db_df.assign(Date="0")
-            klse_all_db_df = klse_all_db_df.assign(ma20d=klse_all_db_df.close_price)
-            klse_all_db_df = klse_all_db_df.assign(ma50d=klse_all_db_df.close_price)
-            klse_all_db_df = klse_all_db_df.assign(Bol_upper=klse_all_db_df.close_price)
-            klse_all_db_df = klse_all_db_df.assign(Bol_lower=klse_all_db_df.close_price)
-            klse_all_db_df = klse_all_db_df.assign(Bol_BW=0.0)
-            klse_all_db_df = klse_all_db_df.assign(Bol_BW_200MA=0.0)
-            klse_all_db_df = klse_all_db_df.assign(exma20d=klse_all_db_df.close_price)
-            klse_all_db_df = klse_all_db_df.assign(exma50d=klse_all_db_df.close_price)
-            
-            #Init date array
-            klse_all_db_df_date_arr = []
-            
-            print("---------------- klse_all_db_df contains")
-            print klse_all_db_df
-            print("---------------- klse_all_db_df ends")
 
             print("---------------- klse_stock_symbol_df contains")
             print klse_stock_symbol_df
             print("---------------- klse_stock_symbol_df ends")
             
-            #print klse_all_db_df['symbol'].size
-            print klse_all_db_df['symbol'].size
-            
-            for i in range( klse_all_db_df['symbol'].size ):        
+            for i in range( klse_stock_symbol_df['symbol'].size ):        
                 try:
-                    print "----------------" + klse_all_db_df['symbol'][i] + "-- construct date ----------------"
-                    print(klse_all_db_df['date_YYYY'][i])
-                    print(klse_all_db_df['date_MM'][i])
-                    print(klse_all_db_df['date_DD'][i])
-                    print(str(klse_all_db_df['date_YYYY'][i])+"/"+str(klse_all_db_df['date_MM'][i])+"/"+str(klse_all_db_df['date_DD'][i]))
-                    klse_all_db_df_date_arr.append(str(klse_all_db_df['date_YYYY'][i]).zfill(4) +"/"+str(klse_all_db_df['date_MM'][i]).zfill(2) +"/"+str(klse_all_db_df['date_DD'][i]).zfill(2) )
-                    klse_all_db_df.iat[i,11] = str(klse_all_db_df['date_YYYY'][i]).zfill(4) +"/"+str(klse_all_db_df['date_MM'][i]).zfill(2) +"/"+str(klse_all_db_df['date_DD'][i]).zfill(2)
-                    print(klse_all_db_df_date_arr)
-                except TypeError as current_error:
-                    print(current_error)
+                    print "----------------" + klse_stock_symbol_df['symbol'][i] + "----------------"
+                    klse_single_stock_df = get_single_stock_db(klse_stock_symbol_df['symbol'][i])
                     
-            print klse_all_db_df
+                    #init empty values
+                    klse_single_stock_df = init_additional_fields_in_df(klse_single_stock_df)
+                    klse_single_stock_df = add_zero_padding_to_date_in_df(klse_single_stock_df)
+                    
+                    klse_single_stock_df['Date'] =  pandas.to_datetime( klse_single_stock_df['Date'], errors='raise', dayfirst=False, yearfirst=True)
+                    temp_data_set = klse_single_stock_df.sort('Date',ascending = True ) #sort to calculate the rolling mean
+                    
+                    temp_data_set['ma20d'] = pandas.rolling_mean(klse_single_stock_df['close_price'], window=20)
+                    temp_data_set['ma50d'] = pandas.rolling_mean(klse_single_stock_df['close_price'], window=50)
+                    temp_data_set['Bol_upper'] = pandas.rolling_mean(klse_single_stock_df['close_price'], window=20) + 2* pandas.rolling_std(klse_single_stock_df['close_price'], 20, min_periods=20)
+                    temp_data_set['Bol_lower'] = pandas.rolling_mean(klse_single_stock_df['close_price'], window=20) - 2* pandas.rolling_std(klse_single_stock_df['close_price'], 20, min_periods=20)
+                    temp_data_set['Bol_BW'] = ((klse_single_stock_df['Bol_upper'] - klse_single_stock_df['Bol_lower'])/klse_single_stock_df['ma20d'])*100
+                    temp_data_set['Bol_BW_200MA'] = pandas.rolling_mean(klse_single_stock_df['Bol_BW'], window=50)#cant get the 200 daa
+                    temp_data_set['Bol_BW_200MA'] = klse_single_stock_df['Bol_BW_200MA'].fillna(method='backfill')##?? ,may not be good
+                    temp_data_set['exma20d'] = pandas.ewma(klse_single_stock_df['close_price'], span=20)
             
-            for i in range( klse_all_db_df['symbol'].size ):        
-                try:
-                    print "----------------" + klse_all_db_df['symbol'][i] + "----------------"
-                    
-                    print "---------------- pt 2 ----------------"
-                    klse_all_db_df['Date'] =  pandas.to_datetime( klse_all_db_df['Date'], errors='raise', dayfirst=False, yearfirst=True)
-                    temp_data_set = klse_all_db_df.sort('Date',ascending = True ) #sort to calculate the rolling mean
-                    
-                    print "---------------- pt 3 ----------------"
-                    temp_data_set['ma20d'] = pandas.rolling_mean(klse_all_db_df['close_price'], window=20)
-                    temp_data_set['ma50d'] = pandas.rolling_mean(klse_all_db_df['close_price'], window=50)
-                    temp_data_set['Bol_upper'] = pandas.rolling_mean(klse_all_db_df['close_price'], window=20) + 2* pandas.rolling_std(klse_all_db_df['close_price'], 20, min_periods=20)
-                    temp_data_set['Bol_lower'] = pandas.rolling_mean(klse_all_db_df['close_price'], window=20) - 2* pandas.rolling_std(klse_all_db_df['close_price'], 20, min_periods=20)
-                    temp_data_set['Bol_BW'] = ((klse_all_db_df['Bol_upper'] - klse_all_db_df['Bol_lower'])/klse_all_db_df['ma20d'])*100
-                    temp_data_set['Bol_BW_200MA'] = pandas.rolling_mean(klse_all_db_df['Bol_BW'], window=50)#cant get the 200 daa
-                    temp_data_set['Bol_BW_200MA'] = klse_all_db_df['Bol_BW_200MA'].fillna(method='backfill')##?? ,may not be good
-                    temp_data_set['exma20d'] = pandas.ewma(klse_all_db_df['close_price'], span=20)
-                    #temp_data_set['exma50d'] = pandas.ewma(temp_data_set['close_price'], span=50)
-                     
-                    #data_ext.all_stock_df.plot(x='Date', y=['close_price','ma20d','ma50d','Bol_upper','Bol_lower' ])
-                    #data_ext.all_stock_df.plot(x='Date', y=['close_price','ma20d','Bol_upper','Bol_lower' ])
-                    #data_ext.all_stock_df.plot(x='Date', y=['Bol_BW','Bol_BW_200MA' ])
-                    
-    
-                    
-    #                print "type of temp_data_set['close_price'] is " + str( type(temp_data_set['close_price']) )
-    #                print "type of temp_data_set['Bol_upper'] is " + str( type(temp_data_set['Bol_upper']) )
-    #                
-    #                print  "temp_data_set['close_price'][0] is " + str( temp_data_set['close_price'][0] )
-    #                print  "temp_data_set['Bol_upper'][0] is " + str( temp_data_set['Bol_upper'][0] )
-                    
-                    print "---------------- pt 4 ----------------"
-                    print(temp_data_set)
-                    if True:#temp_data_set['close_price'][0] > temp_data_set['Bol_upper'][0] :
+                    #print(temp_data_set)
+                    print(temp_data_set['close_price'][temp_data_set['symbol'].size - 1])
+                    print(temp_data_set['Bol_upper'][temp_data_set['symbol'].size - 1])
+                    if temp_data_set['close_price'][temp_data_set['symbol'].size - 1] > temp_data_set['Bol_upper'][temp_data_set['symbol'].size - 1] :
                         print "------------------------------------------------------"
-                        print "---------found   " + klse_all_db_df['name'][i] + " matched-------"
+                        print "---------found   " + klse_stock_symbol_df['symbol'][i] + " matched-------"
                         temp_data_set.plot(x='Date', y=['close_price','ma20d','Bol_upper','Bol_lower' ])
                         plt.show()
                         print "--------------------------------------------------------"
                 except TypeError as current_error:
-                    print "TypeError, skip this stock " + str( klse_all_db_df['symbol'][i] + " error: " + str(current_error))
+                    print "TypeError, skip this stock " + str( klse_stock_symbol_df['symbol'][i] + " error: " + str(current_error))
                     continue
                 except KeyError as current_error:
-                    print "KeyError, skip this stock" + str( klse_all_db_df['symbol'][i] + " error: " + str(current_error))
+                    print "KeyError, skip this stock " + str( klse_stock_symbol_df['symbol'][i] + " error: " + str(current_error))
                     continue
-            
-#        else:
-#            data_ext = YFHistDataExtr()
-#            data_ext.set_interval_to_retrieve(200)#in days
-#            data_ext.set_multiple_stock_list([stock_designated])
-#            data_ext.get_hist_data_of_all_target_stocks()
-#            # convert the date column to date object
-#            print data_ext.all_stock_df
-#            #data_ext.all_stock_df[1] =  pandas.to_datetime( data_ext.all_stock_df[1])
-#            data_ext.all_stock_df["Date"] =  pandas.to_datetime( data_ext.all_stock_df["Date"])
-#            temp_data_set = data_ext.all_stock_df.sort('Date',ascending = True ) #sort to calculate the rolling mean
-#            
-#            temp_data_set['ma20d'] = pandas.rolling_mean(temp_data_set['close_price'], window=20)
-#            temp_data_set['ma50d'] = pandas.rolling_mean(temp_data_set['close_price'], window=50)
-#            temp_data_set['Bol_upper'] = pandas.rolling_mean(temp_data_set['close_price'], window=20) + 2* pandas.rolling_std(temp_data_set['close_price'], 20, min_periods=20)
-#            temp_data_set['Bol_lower'] = pandas.rolling_mean(temp_data_set['close_price'], window=20) - 2* pandas.rolling_std(temp_data_set['close_price'], 20, min_periods=20)
-#            temp_data_set['Bol_BW'] = ((temp_data_set['Bol_upper'] - temp_data_set['Bol_lower'])/temp_data_set['ma20d'])*100
-#            temp_data_set['Bol_BW_200MA'] = pandas.rolling_mean(temp_data_set['Bol_BW'], window=50)#cant get the 200 daa
-#            temp_data_set['Bol_BW_200MA'] = temp_data_set['Bol_BW_200MA'].fillna(method='backfill')##?? ,may not be good
-#            temp_data_set['exma20d'] = pandas.ewma(temp_data_set['close_price'], span=20)
-#            temp_data_set['exma50d'] = pandas.ewma(temp_data_set['close_price'], span=50)
-#            data_ext.all_stock_df = temp_data_set.sort('Date',ascending = False ) #revese back to original
-#             
-#            data_ext.all_stock_df.plot(x='Date', y=['close_price','ma20d','ma50d','Bol_upper','Bol_lower' ])
-#            data_ext.all_stock_df.plot(x='Date', y=['Bol_BW','Bol_BW_200MA' ])
-#            
-#            plt.show()
+                except Exception as e:
+                    print "searching loop error: " + str(e)
+                    continue
         
         end = raw_input("Enter any key to exit: ")
